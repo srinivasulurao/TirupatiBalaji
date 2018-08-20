47edbed698bd30e1a5ee2df877f2b996d774db91
srinivasulurao <n.srinivasulurao@gmail.com>
2018-08-21 03:55:49 +0530

    Custom ListView implementation Success

--- app/src/main/java/com/example/nsrin/tirupatibalaji/CommonListingFragment.java
+++ app/src/main/java/com/example/nsrin/tirupatibalaji/CommonListingFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,8 +35,6 @@ public class CommonListingFragment extends Fragment {
 
     public CommonListingFragment() {
         // Required empty public constructor
-
-
     }
 
     @Override
@@ -43,13 +43,50 @@ public class CommonListingFragment extends Fragment {
 
     }
 
+    //This is an inner class to add our listview.
+    class CustomAdapter extends  BaseAdapter{
+
+        @Override
+        public int getCount(){
+            return articles.length;
+        }
+
+        @Override
+        public Object getItem(int i){
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view=getLayoutInflater().inflate(R.layout.custom_list_view,null);
+
+            ImageView list_view_img=view.findViewById(R.id.list_view_image);
+            TextView list_view_title= view.findViewById(R.id.list_view_title);
+            TextView list_view_description=view.findViewById(R.id.list_view_description);
+
+            list_view_title.setText(articles[position]);
+            list_view_description.setText(articles[position]);
+
+            return view;
+        }
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_common_listing, container, false);
-        ArrayAdapter ListViewAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,articles);
+//      ArrayAdapter ListViewAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,articles);
+        CustomAdapter custom_adapter=new CustomAdapter();
         ListView listView = (ListView) view.findViewById(R.id.article_list);
-        listView.setAdapter(ListViewAdapter);
+        listView.setAdapter(custom_adapter);
+
         return view;
 
     }
@@ -58,4 +95,6 @@ public class CommonListingFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-}
+} //Class ends here.
+
+
